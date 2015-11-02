010e3cd18561440dc2f70e7c5ae4138a2d5aa566
arek <arek968@gmail.com>
2015-11-02 23:02:55 +0100

    Starting point

---
+++ PrimeSA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i=1; i< 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                    Console.WriteLine(string.Format("i= {0}, j= {1}, ixj={2}", i , j, i*j));
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
